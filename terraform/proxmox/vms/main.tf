35716c725f14825191dfff0473d06306d98f30ae
keonji <pavel.shapovalov@emlid.com>
2024-06-26 13:53:30 +0300

    terraform: allocate a wireguard vm

--- terraform/proxmox/vms/main.tf
+++ terraform/proxmox/vms/main.tf
@@ -8,3 +8,14 @@ module "vm-nextcloud" {
   disk_size   = 3800
 #  ip          = "192.168.0.3"
 }
+
+module "vm-wireguard" {
+  source      = "../modules/vm"
+  vmid        = 101
+  name        = "vm-wireguard"
+  target_node = "proxmox"
+  cores       = 2
+  memory      = 2 * 1024
+  disk_size   = 60
+#  ip          = "192.168.0.6"
+}
